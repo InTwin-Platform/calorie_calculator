2c56933b7b15ab211626ad1e04ddd40b9f51fa3b
Mohamed Refaat <mohamedrefaat900@gmail.com>
2025-09-16 18:31:55 +0300

    Fix package name to use hyphens and correct author name in setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="caloric_calculator",
+    name="calorie-calculator",
     version="1.0.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    py_modules=["caloric_calculator"],
-    author="Intwin",
+    author="InTwin",
     author_email="intixel.intwin@gmail.com",
     description="Caloric needs calculator",
     python_requires=">=3.7",
