d9c95460f54868c87c9c156e6f9854213bcf8651
Mohamed Refaat <mohamedrefaat900@gmail.com>
2025-09-16 18:24:16 +0300

    Implement caloric calculator functionality and restructure package setup

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="caloric_calculator",
     version="1.0.0",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     py_modules=["caloric_calculator"],
     author="Intwin",
     author_email="intixel.intwin@gmail.com",
---
+++ src/caloric_calculator/__init__.py
@@ -0,0 +1,5 @@
+from .calculator import CaloricCalculator
+from .models import WeightGoal
+
+__version__ = "1.0.0"
+__all__ = ["CaloricCalculator", "WeightGoal"]
